ce9b8fab9efade349cefb73caad5668d968bd916
Cloudymolecule <baires82@gmail.com>
2021-02-18 11:02:32 -0500

    commit tree bcd48993bfa06e3137998791202b12cb848f66f1

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    // stuff = document.getElementById('text')
+    // stuff.innerHTML = "This is really cool!"
+
+    document.getElementById('text').innerHTML = "This is really cool!"
+})
